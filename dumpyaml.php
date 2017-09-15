32ee17faf51fdb8feec85491ad5955facf4a8347
akramfares <akramfares@gmail.com>
2017-09-15 10:09:29 -0700

    Modify mysql functions to mysqli

--- dumpyaml.php
+++ dumpyaml.php
@@ -3,7 +3,7 @@
 /* ========================================================
  * File           : dumpyaml.php
  * Date           : July 15, 2010, Edited December 5, 2014
- * Author         : Jonathan Franzone, Edited by: Ben Robie
+ * Author         : Jonathan Franzone, Edited by: Ben Robie and Akram Fares
  * Author Website : Original Script: http://www.franzone.com/2010/07/16/dump-a-mysql-database-to-yaml-using-php/
  *
  * Script to generate YAML (http://www.yaml.org/) from
@@ -21,6 +21,7 @@
  * Configuration (read from command-line)
  * ========================================================
  */
+
 $DBHOST = '';
 $DBUSER = '';
 $DBPASS = '';
@@ -34,7 +35,7 @@ $CONVERT_NAME = false;
  * Prints the usage for this script
  */
 function usage() {
-  echo "
+	echo "
 MySQLToYaml
 
   Script to generate YAML (http://www.yaml.org/) from
@@ -64,66 +65,66 @@ Usage : dumpyaml.php [options] -d <database>
  */
 function read_args() {
 
-  global $argc, $argv, $DBHOST, $DBUSER, $DBPASS, $DBNAME, $TABLES, $DBQUERY, $QUERYNAME, $CONVERT_NAMES;
+	global $argc, $argv, $DBHOST, $DBUSER, $DBPASS, $DBNAME, $TABLES, $DBQUERY, $QUERYNAME, $CONVERT_NAMES;
 
-  while(!empty($argv)) {
+	while(!empty($argv)) {
 
-    $arg = array_shift($argv);
-    switch($arg) {
-      case '-h':
-        $DBHOST = array_shift($argv);
-        break;
-      case '-u':
-        $DBUSER = array_shift($argv);
-        break;
-      case '-p':
-        $DBPASS = array_shift($argv);
-        break;
-      case '-d':
-        $DBNAME = array_shift($argv);
-        break;
-      case '-t':
-        $TABLES = explode(",", array_shift($argv));
-        break;
-      case '-q':
-        $DBQUERY = array_shift($argv);
-        break;
-      case '-qn':
-        $QUERYNAME = array_shift($argv);
-        break;
-      case '-n':
-        $CONVERT_NAMES = true;
-        break;
-    }
-  }
+		$arg = array_shift($argv);
+		switch($arg) {
+			case '-h':
+				$DBHOST = array_shift($argv);
+				break;
+			case '-u':
+				$DBUSER = array_shift($argv);
+				break;
+			case '-p':
+				$DBPASS = array_shift($argv);
+				break;
+			case '-d':
+				$DBNAME = array_shift($argv);
+				break;
+			case '-t':
+				$TABLES = explode(",", array_shift($argv));
+				break;
+			case '-q':
+				$DBQUERY = array_shift($argv);
+				break;
+			case '-qn':
+				$QUERYNAME = array_shift($argv);
+				break;
+			case '-n':
+				$CONVERT_NAMES = true;
+				break;
+		}
+	}
 
-  // Validate Command-Line Arguments
-  $retVal = true;
-  if (empty($DBHOST)) {
-    echo "-h <host> is required\n";
-    $retVal = false;
-  }
-  if (empty($DBUSER)) {
-    echo "-u <user> is required\n";
-    $retVal = false;
-  }
-  if (empty($DBPASS)) {
-    echo "-p <pass> is required\n";
-    $retVal = false;
-  }
-  if (empty($DBNAME)) {
-    echo "-d <database> is required\n";
-    $retVal = false;
-  }
-  if (!empty($TABLES) && !empty($DBQUERY)) {
-    echo "-t <table(s)> and -q \"<query>\" are mutually exclusive options\n";
-    $retVal = false;
-  }
-  if (!empty($DBQUERY) && empty($QUERYNAME)) {
-    echo "-qn <query name> is required when using -q \"<query>\"\n";
-    $retVal = false;
-  }
-  return $retVal;
+	// Validate Command-Line Arguments
+	$retVal = true;
+	if (empty($DBHOST)) {
+		echo "-h <host> is required\n";
+		$retVal = false;
+	}
+	if (empty($DBUSER)) {
+		echo "-u <user> is required\n";
+		$retVal = false;
+	}
+	if (empty($DBPASS)) {
+		echo "-p <pass> is required\n";
+		$retVal = false;
+	}
+	if (empty($DBNAME)) {
+		echo "-d <database> is required\n";
+		$retVal = false;
+	}
+	if (!empty($TABLES) && !empty($DBQUERY)) {
+		echo "-t <table(s)> and -q \"<query>\" are mutually exclusive options\n";
+		$retVal = false;
+	}
+	if (!empty($DBQUERY) && empty($QUERYNAME)) {
+		echo "-qn <query name> is required when using -q \"<query>\"\n";
+		$retVal = false;
+	}
+	return $retVal;
 }
 
 /**
@@ -132,19 +133,19 @@ function read_args() {
  */
 function convert_name_to_class($string) {
 
-  // Return Value
-  $retVal = '';
+	// Return Value
+	$retVal = '';
 
-  $tokens = explode('_', $string);
-  foreach($tokens as $token) {
-    $retVal .= strtoupper(substr($token, 0, 1));
-    if (strlen($token) > 1) {
-      $retVal .= substr($token, 1);
-    }
-  }
+	$tokens = explode('_', $string);
+	foreach($tokens as $token) {
+		$retVal .= strtoupper(substr($token, 0, 1));
+		if (strlen($token) > 1) {
+			$retVal .= substr($token, 1);
+		}
+	}
 
-  // Return Value
-  return $retVal;
+	// Return Value
+	return $retVal;
 }
 
 /**
@@ -152,41 +153,41 @@ function convert_name_to_class($string) {
  */
 function sql_to_yaml($link, $sql, $table) {
 
-  global $CONVERT_NAMES;
+	global $CONVERT_NAMES;
 
-  // Run the query
-  $result = mysql_query($sql, $link);
+	// Run the query
+	$result = mysqli_query($link, $sql);
 
-  if ($result) {
+	if ($result) {
 
-    // Output the table name
-    echo ($CONVERT_NAMES === true) ? convert_name_to_class($table) . ":\n" : "  {$table}:\n";
+		// Output the table name
+		echo ($CONVERT_NAMES === true) ? convert_name_to_class($table) . ":\n" : "{$table}:\n";
 
-    // Loop over the result set
-    while ($row = mysql_fetch_assoc($result)) {
-      $first = true;
-      // Output the row/collection indicator
-      echo "    - ";
+		// Loop over the result set
+		while ($row = mysqli_fetch_assoc($result)) {
+			$first = true;
+			// Output the row/collection indicator
+			echo "  - ";
 
-      // Loop over the columns output names and values
-      foreach ($row as $key => $value) {
+			// Loop over the columns output names and values
+			foreach ($row as $key => $value) {
 
-        // Do have any newlines or line feeds?
-        $literalFlag = (strpos($value, "\r") !== FALSE || strpos($value, "\n") !== FALSE) ? "| " : "";
+				// Do have any newlines or line feeds?
+				$literalFlag = (strpos($value, "\r") !== FALSE || strpos($value, "\n") !== FALSE) ? "| " : "";
 
-        // Output the key/value pair
-        if ($first){
-		echo "{$key}: {$literalFlag}{$value}\n";
-		$first = false;
-        } else {
-		echo "      {$key}: {$literalFlag}{$value}\n";
+				// Output the key/value pair
+				if ($first){
+					echo "{$key}: {$literalFlag}{$value}\n";
+					$first = false;
+				} else {
+					echo "      {$key}: {$literalFlag}{$value}\n";
+				}
+			}
+		}
 	}
-      }
-    }
-  }
 
-  // Free the result resources
-  mysql_free_result($result);
+	// Free the result resources
+	mysqli_free_result($result);
 }
 
 /**
@@ -194,20 +195,20 @@ function sql_to_yaml($link, $sql, $table) {
  */
 function get_database_tables($link) {
 
-  global $TABLES;
+	global $TABLES;
 
-  // Run the query
-  $result = mysql_query('SHOW TABLES', $link);
+	// Run the query
+	$result = mysqli_query('SHOW TABLES', $link);
 
-  if ($result) {
+	if ($result) {
 
-    while ($row = mysql_fetch_row($result)) {
-      $TABLES[] = $row[0];
-    }
-  }
+		while ($row = mysqli_fetch_row($result)) {
+			$TABLES[] = $row[0];
+		}
+	}
 
-  // Free the result resources
-  mysql_free_result($result);
+	// Free the result resources
+	mysqli_free_result($result);
 }
 
 /**
@@ -215,49 +216,43 @@ function get_database_tables($link) {
  */
 if (read_args()) {
 
-  // Open database connection
-  $link = mysql_connect($DBHOST, $DBUSER, $DBPASS);
-  if (!$link) {
-    die("Could not connect to DB [{$DBUSER}:{$DBPASS}@{$DBHOST}] :: " . mysql_error()) . "\n";
-  }
+	// Open database connection
+	$link = mysqli_connect($DBHOST, $DBUSER, $DBPASS, $DBNAME, 6603);
+	if (!$link) {
+		die("Could not connect to DB [{$DBUSER}:{$DBPASS}@{$DBHOST}]\n");
+	}
 
-  // Select the database
-  if (mysql_select_db($DBNAME)) {
+	// Select the database
+	if (mysqli_select_db($link, $DBNAME)) {
 
-    // Output header
-    echo "tables:\n";
+		// Query Mode
+		if (!empty($DBQUERY)) {
+			sql_to_yaml($link, $DBQUERY, $QUERYNAME);
+		}
 
-    // Query Mode
-    if (!empty($DBQUERY)) {
-      sql_to_yaml($link, $DBQUERY, $QUERYNAME);
-    }
+		else {
 
-    else {
+			// If the user has not specified tables then query for them
+			if (empty($TABLES)) {
+				get_database_tables($link);
+			}
 
-      // If the user has not specified tables then query for them
-      if (empty($TABLES)) {
-        get_database_tables($link);
-      }
+			// Loop over tables and output YAML
+			foreach ($TABLES as $tbl) {
+				sql_to_yaml($link, "SELECT * FROM {$tbl}", $tbl);
+				echo "\n";
+			}
+		}
+	}
+	else {
+		echo "Could not select database [{$DBNAME}] :: " . mysqli_error() . "\n";
+	}
 
-      // Loop over tables and output YAML
-      foreach ($TABLES as $tbl) {
-        sql_to_yaml($link, "SELECT * FROM {$tbl}", $tbl);
-        echo "\n";
-      }
-    }
-
-    // Output footer
-    //echo "...\n";
-  }
-  else {
-    echo "Could not select database [{$DBNAME}] :: " . mysql_error() . "\n";
-  }
-
-  // Close Database
-  mysql_close($link);
+	// Close Database
+	mysqli_close($link);
 }
 else
 {
-  usage();
+	usage();
 }
 ?>
